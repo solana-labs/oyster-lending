a123eefbd31ef245144da861794e03f81f86d562
bartosz-lipinski <264380+bartosz-lipinski@users.noreply.github.com>
2020-12-22 00:01:17 -0600

    Updates

---
+++ precache-manifest.3c27738a44e32893cc2fe3f27e79463c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b9fe95f26f49a9b1f6733c22706d924",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c26d786b2c458f456895",
+    "url": "./static/css/2.d1dfd3d6.chunk.css"
+  },
+  {
+    "revision": "373baa7cd0b5ea9ce911",
+    "url": "./static/css/main.63ee6ee1.chunk.css"
+  },
+  {
+    "revision": "c26d786b2c458f456895",
+    "url": "./static/js/2.38808d2a.chunk.js"
+  },
+  {
+    "revision": "20eed7947fe51c9d6e31c3740ccda59e",
+    "url": "./static/js/2.38808d2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "373baa7cd0b5ea9ce911",
+    "url": "./static/js/main.a3fcac35.chunk.js"
+  },
+  {
+    "revision": "f4270ecf90b56311f2d1",
+    "url": "./static/js/runtime-main.0ad816d2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a3a694faa9cc5cae83a8f34fc440943c.js"
+  "./precache-manifest.3c27738a44e32893cc2fe3f27e79463c.js"
 );
 
 self.addEventListener('message', (event) => {
