fa68a2c4271cca3188452f4f36f60a0f9ed2c7f4
bartosz-lipinski <264380+bartosz-lipinski@users.noreply.github.com>
2020-12-22 17:31:18 -0600

    Updates

---
+++ precache-manifest.7f826e5aec61cefa1f68207c599da2d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6854844cb4a7d6ab96410762ab34a1e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bfcba7990bf7a9d51511",
+    "url": "./static/css/2.d1dfd3d6.chunk.css"
+  },
+  {
+    "revision": "3d1096cf4268ecfc93b9",
+    "url": "./static/css/main.00db5823.chunk.css"
+  },
+  {
+    "revision": "bfcba7990bf7a9d51511",
+    "url": "./static/js/2.a45dc55f.chunk.js"
+  },
+  {
+    "revision": "20eed7947fe51c9d6e31c3740ccda59e",
+    "url": "./static/js/2.a45dc55f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d1096cf4268ecfc93b9",
+    "url": "./static/js/main.cbcd864d.chunk.js"
+  },
+  {
+    "revision": "f4270ecf90b56311f2d1",
+    "url": "./static/js/runtime-main.0ad816d2.js"
+  }
+]);
